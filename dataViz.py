e6038a9fbc9e7f070d9ed8929a6d3928bbf0324d
Arushi789 <63102065+Arushi789@users.noreply.github.com>
2021-03-05 17:16:00 +0530

    Add files via upload

---
+++ dataViz.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import plotly.express as px
+
+#data= [1,2,3,4,5]
+df=pd.read_csv("line_chart.csv")
+fig= px.line(df,x="Year",y="Per capita income",color="Country",title="Per Capita Income")
+fig.show()
